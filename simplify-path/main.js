ad7b2321135cecb4c39094b0d4f4c3f81a85156b
Lesetja <kmalapane155@gmail.com>
2025-10-09 22:42:14 +0200

    final solution

--- simplify-path/main.js
+++ simplify-path/main.js
@@ -10,22 +10,19 @@
  */
 let simplifyPath = function (path = "") {
   let pathDirs = path.split("/");
-
+  
   pathDirs = pathDirs.filter(dir => dir != "")
   pathDirs = pathDirs.filter(dir => dir != ".")
 
-  for (let i = 0; i < pathDirs.length; i++) {
-    if (pathDirs[i] == "..") {
-      pathDirs = pathDirs.slice(0, i-1).concat(pathDirs.slice(i+1, pathDirs.length))
-    }
-    
-    if (i > 0 && i <= pathDirs.length - 1 && pathDirs[i] == "") {
-        pathDirs = pathDirs.slice(0, i).concat(pathDirs.slice(i+1, pathDirs.length))
-    }
+  while (pathDirs.includes("..")) {
+    let i = pathDirs.indexOf("..")
+    pathDirs = pathDirs.slice(0, i-1).concat(pathDirs.slice(i+1, pathDirs.length))
   }
-
+  
   return "/" + pathDirs.join("/");
 };
 
-console.log(simplifyPath("/.../a/../b/c/../d/./"));
-console.log(simplifyPath("/home//foo/"))
+console.log(simplifyPath("/a/./b/../../c/"))
+
+// console.log(simplifyPath("/.../a/../b/c/../d/./"))
+// console.log(simplifyPath("/home//foo/"))
