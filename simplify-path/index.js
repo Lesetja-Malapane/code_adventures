0d88826ca4d6a8e9b2d7d97d76b2beee173dd832
Lesetja <kmalapane155@gmail.com>
2025-10-09 22:16:16 +0200

    solution for '//' (double /)

--- simplify-path/index.js
+++ simplify-path/index.js
@@ -10,10 +10,15 @@
  */
 let simplifyPath = function (path = "") {
   let pathDirs = path.split("/");
+  console.log(pathDirs)
 
   for (let i = 0; i < pathDirs.length; i++) {
     if (pathDirs[i] == "..") {
         pathDirs = pathDirs.slice(0, i-1).concat(pathDirs.slice(i+1, pathDirs.length))
+    }
+
+    if (i > 0 && i <= pathDirs.length - 1 && pathDirs[i] == "") {
+        pathDirs = pathDirs.slice(0, i).concat(pathDirs.slice(i+1, pathDirs.length))
     }
   }
 
@@ -31,3 +36,4 @@ let simplifyPath = function (path = "") {
 };
 
 console.log(simplifyPath("/.../a/../b/c/../d/./"));
+console.log(simplifyPath("/home//foo/"))
